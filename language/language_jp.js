b4384267ea575ce4845c7cbed643b5894e342785
dengbb <804433988@qq.com>
2019-01-23 11:00:37 +0800

    优点

--- language/language_jp.js
+++ language/language_jp.js
@@ -10,5 +10,17 @@ var data_jp = {
 
   Regulated: "Regulated",
   Professional: "Professional",
-  Reliable: "Reliable"
+  Reliable: "Reliable",
+
+  advantage: "STOのメリット",
+  title_1: "24時間356日",
+  detail_1: "伝統的な証券取引所とは違い、STO取引所は休むことなく稼働が可能",
+  title_2: "低取引コスト",
+  detail_2:
+    "配当・譲渡・移転がスマートコントラクトで自動化されるため取引コストが低い",
+  title_3: "グローバル市場",
+  detail_3:
+    "ネットワーク上で取引されるトークンで世界中から資金を呼び込むことが可能",
+  title_4: "適法性",
+  detail_4: "各国の証券法に準拠した形で発行され資産管理と運用がより安全に"
 };
