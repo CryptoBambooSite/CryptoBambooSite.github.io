d8ffae527a43193ecd514885fded6dfe7da33238
dengbb <804433988@qq.com>
2019-01-23 10:13:25 +0800

    文字替换

--- language/language_jp.js
+++ language/language_jp.js
@@ -2,5 +2,13 @@ var data_jp = {
   home: "ホーム ",
   "Crypto Bamboo": "Crypto Bamboo",
   solution: "ソリューション",
-  "contact us": "お問い合わせ "
+  "contact us": "お問い合わせ ",
+  Visible: "Visible",
+
+  "More Safety": "More Safety",
+  info_one: "日本発\nSTO特化型のマルチプラットフォーム",
+
+  Regulated: "Regulated",
+  Professional: "Professional",
+  Reliable: "Reliable"
 };
